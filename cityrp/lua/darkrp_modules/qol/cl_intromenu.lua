7570978e12781e8ee7070ec8de38aa4a766b6d39
SirFrancisBillard <foresome1249@gmail.com>
2017-02-26 20:59:18 -0800

    Intro menu fixes

--- cityrp/lua/darkrp_modules/qol/cl_intromenu.lua
+++ cityrp/lua/darkrp_modules/qol/cl_intromenu.lua
@@ -1,7 +1,7 @@
 INTROMENU = {}
 
 surface.CreateFont("IntroMenu_ServerTitleFont", {
-	size = (type(ScrH) == "function" and ScrH() or 1080) / 18,
+	size = (type(ScrH) == "function" and ScrH() or 1080) / 24,
 	weight = 800,
 	antialias = true,
 	shadow = false,
@@ -9,7 +9,6 @@ surface.CreateFont("IntroMenu_ServerTitleFont", {
 })
 
 function INTROMENU:Build()
-	self:Init()
 	self:Destroy()
 
 	self.Panel = vgui.Create("DPanel")
@@ -17,25 +16,30 @@ function INTROMENU:Build()
 	self.Panel:Center()
 	self.Panel:MakePopup()
 	self.Panel.Paint = function(pnl, w, h)
-		draw.RoundedBox(0, 0, 0, w, h, Color(150, 150, 150, 100))
+		draw.RoundedBox(0, 0, 0, w, h, Color(50, 50, 50, 200))
 	end
 
 	local ServerTitle = vgui.Create("DLabel", self.Panel)
 	ServerTitle:SetFont("IntroMenu_ServerTitleFont")
-	ServerTitle:SetPos(ScrW() / 2, (ScrH() / 2) * 0.5)
-	ServerTitle:SetText("Welcome to " .. GetHostName() .. ", " .. LocalPlayer():Nick() .. ".")
+	ServerTitle:SetSize(ScrW(), ScrH() / 5)
+	
+	local text = "Welcome to " .. GetHostName() .. ", " .. LocalPlayer():Nick() .. "."
+	surface.SetFont("IntroMenu_ServerTitleFont")
+	local offset = surface.GetTextSize(text) / 2
+	ServerTitle:SetText(text)
+	ServerTitle:SetPos(ScrW() / 2 - offset, (ScrH() / 2) * 0.3)
 
 	local CloseButton = vgui.Create("DButton", self.Panel)
 	CloseButton:SetText("Close")
-	CloseButton:SetPos(ScrW() / 2, (ScrH() / 2) * 1.5)
-	CloseButton:SetSize(ScrH() * 0.1, ScrW() * 0.2)
+	CloseButton:SetPos(ScrW() / 2 - ScrW() * 0.1, (ScrH() / 2) * 1.5)
+	CloseButton:SetSize(ScrW() * 0.2, ScrH() * 0.1)
 	CloseButton.DoClick = function()
 		self:Destroy()
 	end
 end
 
 function INTROMENU:Destroy()
-	if IsValid(INTROMENU.Panel) and type(INTROMENU.Panel.Remove) == "function" then]
+	if IsValid(INTROMENU.Panel) and type(INTROMENU.Panel.Remove) == "function" then
 		INTROMENU.Panel:Remove()
 	end
 end
