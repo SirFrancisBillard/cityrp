32cace7c5d689ecb85e0357b2bae7a2c0d7efb11
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-03-03 15:43:42 -1000

    Molotov entity

---
+++ cityrp/lua/entities/ent_molotov.lua
@@ -0,0 +1,47 @@
+AddCSLuaFile()
+
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+
+ENT.PrintName		= "Thrown Molotov"
+
+if CLIENT then return end
+
+function ENT:Initialize()
+	self:SetModel("models/props_junk/garbage_glassbottle003a.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetAngles(Angle(math.random(0, 360), math.random(0, 360), math.random(0, 360)))
+	
+	local Phys = self:GetPhysicsObject()
+	if not Phys or not Phys:IsValid() then return end
+	self:GetPhysicsObject():Wake()
+	self.What = CurTime()
+end
+
+function ENT:Explode(pos)
+	sound.Play("physics/glass/glass_cup_break".. math.random(1, 2).. ".wav", self:GetPos(), 150, 150)
+
+	local fire = ents.Create("ent_fire")
+	fire:SetPos(pos)
+	fire:Spawn()
+	self:Remove()
+end
+
+function ENT:Think()
+	local trace = {}
+	trace.start = self:GetPos()
+	trace.endpos = self:GetPos() + self:GetVelocity() * 5
+	trace.filter = self
+	
+	if self.What + 10 < CurTime() then
+		self:Explode(self:GetPos())
+		return false
+	end
+	
+	local tr = util.TraceLine(trace)
+	if tr.Hit and tr.HitPos:Distance(self:GetPos()) < 75 then
+		self:Explode(tr.HitPos)
+	end
+end
