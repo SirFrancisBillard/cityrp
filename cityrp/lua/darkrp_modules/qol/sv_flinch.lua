a321f61dc081bf7050ecfce777c98fdeb057d22c
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-03-06 09:20:08 -1000

    flinch only on headshot

--- cityrp/lua/darkrp_modules/qol/sv_flinch.lua
+++ cityrp/lua/darkrp_modules/qol/sv_flinch.lua
@@ -1,8 +1,9 @@
 
 util.AddNetworkString("ShowPlayerFlinch")
 
-hook.Add("EntityTakeDamage", "BetterBulletDamage", function(ent, dmg)
-	if IsValid(ent) and ent:IsPlayer() then
+hook.Add("EntityTakeDamage", "PlayerFlinchOnHeadshot", function(ent, dmg)
+	if IsValid(ent) and ent:IsPlayer() and ent:LastHitgroup() == HITGROUP_HEAD then
+		ent:AnimRestartGesture(GESTURE_SLOT_FLINCH, ACT_FLINCH_HEAD, true)
 		net.Start("ShowPlayerFlinch")
 		net.WriteInt(ent:EntIndex(), 8)
 		net.Broadcast()
