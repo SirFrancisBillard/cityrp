1da2f94a119831a5e1020ec39126689493ef1cae
Sir Francis Billard <sirfrancisbillard@gmail.com>
2017-03-06 16:03:01 -1000

    c4 formatting

--- cityrp/lua/weapons/weapon_c4.lua
+++ cityrp/lua/weapons/weapon_c4.lua
@@ -5,36 +5,34 @@ if CLIENT then
 	language.Add("C4_ammo", "Plastic Explosives")
 end
 
-SWEP.PrintName 					= "C4"
-SWEP.Slot 						= 4
-SWEP.SlotPos 					= 1
-SWEP.DrawAmmo 					= false
-SWEP.DrawCrosshair 				= false
+SWEP.PrintName = "C4"
+SWEP.Slot = 4
+SWEP.SlotPos = 1
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = false
 
-SWEP.Instructions 				= ""
-SWEP.Author 					= ""
-SWEP.Contact 					= ""
-SWEP.Purpose 					= "Blow shit up."
+SWEP.Instructions = "<color=green>[PRIMARY FIRE]</color> Plant C4 on a nearby surface."
+SWEP.Purpose = "Destruction"
 
 SWEP.UseHands = true
-SWEP.ViewModel				= "models/weapons/cstrike/c_c4.mdl"	-- Weapon view model
-SWEP.WorldModel				= "models/weapons/w_c4.mdl"	-- Weapon world model
+SWEP.ViewModel = "models/weapons/cstrike/c_c4.mdl"
+SWEP.WorldModel = "models/weapons/w_c4.mdl"
 
-SWEP.ViewModelFOV 				= 54
-SWEP.ViewModelFlip 				= false
+SWEP.ViewModelFOV = 54
+SWEP.ViewModelFlip = false
 
-SWEP.Spawnable 					= true
-SWEP.Category 					= "RP"
+SWEP.Spawnable = true
+SWEP.Category = "RP"
 
-SWEP.Primary.ClipSize 			= -1
-SWEP.Primary.DefaultClip 		= 1
-SWEP.Primary.Automatic 			= false
-SWEP.Primary.Ammo 				= "C4"
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = 1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "C4"
 
-SWEP.Secondary.ClipSize 		= -1
-SWEP.Secondary.DefaultClip 		= -1
-SWEP.Secondary.Automatic 		= false
-SWEP.Secondary.Ammo 			= ""
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = ""
 
 function SWEP:Initialize()
 	self:SetHoldType("slam")
@@ -42,13 +40,13 @@ end
 
 function SWEP:PrimaryAttack()
 	self:SetNextPrimaryFire(CurTime() + 0.6)
-	if (SERVER) then
+	if SERVER then
 		local tr = util.TraceLine({
 			start = self.Owner:GetShootPos(),
 			endpos = self.Owner:GetShootPos() + 100 * self.Owner:GetAimVector(),
-			filter = {self.Owner},
+			filter = {self.Owner}
 		})
-	
+
 		local c4 = ents.Create("ent_c4")
 		c4:SetPos(tr.HitPos)
 		c4:SetAngles(tr.HitNormal:Angle() - Angle(90, 180, 0))
@@ -67,7 +65,7 @@ function SWEP:PrimaryAttack()
 		end
 
 		hook.Call("PlayerPlaceC4", nil, self.Owner, tr.Entity)
-			
+
 		if not tr.Hit then
 			c4:SetMoveType(MOVETYPE_VPHYSICS)
 		end
